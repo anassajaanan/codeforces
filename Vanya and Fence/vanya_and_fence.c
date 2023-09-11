74aa7711cc158970d9664bfc83806cd4c522edb1
Anas Ajaanan <aajaanan@lab1r3s13.42abudhabi.ae>
2023-09-11 09:44:34 +0400

    Vanya and Fence

---
+++ Vanya and Fence/vanya_and_fence.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+int main(void)
+{
+	int	x;
+	int	friends;
+	int	fence_height;
+	int	total_width;
+
+	total_width = 0;
+	scanf("%d %d", &friends, &fence_height);
+	for (int i = 0; i < friends; i++)
+	{
+		scanf("%d", &x);
+		if (x > fence_height)
+			total_width += 2;
+		else
+			total_width += 1;
+	}
+	printf("%d\n", total_width);
+	return (0);
+}
